1cda57d06c58aa7d3d04d0e143c6d2286bf6dd4f
JunH <tjdtls690@gmail.com>
2023-02-18 11:52:25 +0900

    :art: refactor : 예외 처리하는 메서드의 구현부에서 굳이 분리가 필요하지 않은 코드들 다시 가져오기

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -18,26 +18,16 @@ public class Car {
     }
     
     private void validate(String name) {
-        validateNullAndEmpty(name);
-        validateLength(name);
-        validateEachNameBlank(name);
-    }
-    
-    private void validateEachNameBlank(String name) {
-        if (name.contains(BLANK)) {
-            throw new IllegalArgumentException("이름에는 공백을 입력할 수 없습니다.");
+        if (Objects.isNull(name) || name.isBlank()) {
+            throw new IllegalArgumentException("빈 문자열 및 공백은 입력할 수 없습니다.");
         }
-    }
     
-    private void validateLength(String name) {
         if (isCorrectLength(name)) {
             throw new IllegalArgumentException("1글자 이상 5글자 이하로 입력하세요.");
         }
-    }
     
-    private void validateNullAndEmpty(String name) {
-        if (Objects.isNull(name) || name.isBlank()) {
-            throw new IllegalArgumentException("빈 문자열 및 공백은 입력할 수 없습니다.");
+        if (name.contains(BLANK)) {
+            throw new IllegalArgumentException("이름에는 공백을 입력할 수 없습니다.");
         }
     }
     
